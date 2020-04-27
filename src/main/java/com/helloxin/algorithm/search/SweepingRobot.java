a6515c795bac88a5424bea4a03cbae7853fdfa0a
叶信节 <yexinjie@souche.com>
2020-04-27 14:11:44 +0800

    MODIFY 去掉没用的部分

--- src/main/java/com/helloxin/algorithm/search/SweepingRobot.java
+++ src/main/java/com/helloxin/algorithm/search/SweepingRobot.java
@@ -31,7 +31,6 @@ public class SweepingRobot {
     private static int moveNext(int nextStep, List<int[]> steps) {
         //走完 N 步，返回 1，增加一次走法
         if (steps.size() == N + 1) {
-            show(steps);
             return 1;
         }
         int cnt = 0;
@@ -72,78 +71,8 @@ public class SweepingRobot {
         }
         return result;
     }
-
-    /**
-     * 路径打印
-     *
-     * @param steps
-     */
-    private static void show(List<int[]> steps) {
-        int min_y = 0;//最小纵坐标
-        int max_y = 0;//最大纵坐标
-        int min_x = 0;//最小横坐标
-        for (int i = 0; i < steps.size(); i++) {
-            if (steps.get(i)[1] < min_y) {
-                min_y = steps.get(i)[1];
-            }
-            if (steps.get(i)[1] > max_y) {
-                max_y = steps.get(i)[1];
-            }
-            if (steps.get(i)[0] < min_x) {
-                min_x = steps.get(i)[0];
-            }
-        }
-        int y = max_y - min_y + 1;//纵坐标（路径多少行）
-        ArrayList<Node>[] arrayLists = new ArrayList[y];
-        for (int i = 0; i < steps.size(); i++) {
-            int y_v = steps.get(i)[1];//纵坐标
-            int x_v = steps.get(i)[0] - min_x + 1;//横坐标
-            Node node = new Node(i, x_v);
-            ArrayList<Node> arrayNode = arrayLists[y_v - min_y];
-            if (arrayNode == null)
-                arrayNode = new ArrayList<>();
-            arrayNode.add(node);
-            arrayNode.sort(new SortByX());
-            arrayLists[y_v - min_y] = arrayNode;
-        }
-        printToConsole(arrayLists);
-    }
-
-    /**
-     * 打印到console
-     *
-     * @param arrayLists
-     */
-    private static void printToConsole(ArrayList<Node>[] arrayLists) {
-        int length = String.valueOf(N).length() + 1;//取N的长度+1 方便观察和对齐
-        String blank = "";
-        for (int i = 0; i < length; i++) {
-            blank += " ";
-        }
-        for (ArrayList<Node> nodes : arrayLists) {
-            for (int i = 0; i < nodes.size(); i++) {
-                int count = i == 0 ? nodes.get(i).getX()
-                        : nodes.get(i).getX() - nodes.get(i - 1).getX();
-                for (int j = 1; j < count; j++) {
-                    System.out.print(blank);
-                }
-                String v = String.valueOf(nodes.get(i).getV());
-                int v_l = v.length();
-                if (v_l < length)
-                    for (int j = 0; j < length - v_l; j++) {
-                        v = " " + v;//位数不够，前补空格
-                    }
-                System.out.print(v);
-            }
-            System.out.println("");
-        }
-        String cut_of = "";
-        for (int i = 0; i <= N; i++) {
-            cut_of += "***";
-        }
-        System.out.println(cut_of);//分割线
-    }
 }
+
 
 class Node {
     private int v;// 值
